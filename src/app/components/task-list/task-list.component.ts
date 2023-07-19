5dd1fcb1244e7f60bb5a86f2daac4e25afe3625d
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2023-07-18 20:19:04 -0400

    update dragdrop to have smooth drag between status

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -29,12 +29,7 @@ export class TaskListComponent implements OnInit
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
+      event.item.data.status = event.container.id;
 
       this.taskService.updateStatus(event.item.data.id, event.container.id);
     }
